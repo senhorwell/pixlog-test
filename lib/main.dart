703a6d466c3205b64d308dab1f4ea860c8bf2100
Well Luiz <wellingtonluiiiz@gmail.com>
2022-05-18 14:38:11 -0300

    Desenvolvimento de splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pixlog_translater/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pixlog Translater',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Pixlog Translater'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Você apertou o botão:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: SafeArea(child: SplashPage())
     );
   }
 }
---
+++ lib/pages/splash.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'dart:async';
+import 'package:pixlog_translater/pages/home.dart';
+import 'package:pixlog_translater/themes/app_colors.dart';
+import 'package:pixlog_translater/themes/app_images.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _animation = Tween(
+      begin: 3.0,
+      end: 0.0,
+    ).animate(_controller);
+    super.initState();
+
+    _mockCheckForSession();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<bool> _mockCheckForSession() async {
+    await Future.delayed(Duration(milliseconds: 3000), () {});
+    _navigateToHome();
+
+    return true;
+  }
+
+  void _navigateToHome() {
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => HomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.forward();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: AppColors.primary,
+        ),
+        child: Center(
+            child: Hero(
+          tag: "heroLogo",
+          child: FadeTransition(
+            opacity: Tween(
+              begin: 0.0,
+              end: 1.0,
+            ).animate(_controller),
+            child: Center(
+              child: Image.asset(
+                AppImages.logo,
+                width: 200,
+              ),
+            ),
+          ),
+        )),
+      ),
+    );
+  }
+}
