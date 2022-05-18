1fe9355da2115b6b448e043aff50cd32f927e1b8
Well Luiz <wellingtonluiiiz@gmail.com>
2022-05-18 16:06:00 -0300

    Atualizando cores

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xFF132B39);
   static const Color background = Color(0xFFEEEEEE);
+  static const Color searchIcon = Color(0xFF1A73E8);
 }
