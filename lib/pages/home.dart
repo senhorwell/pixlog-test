dad7afbed9813af2ff5502fec629821468dd1fa3
Well Luiz <wellingtonluiiiz@gmail.com>
2022-05-18 15:15:26 -0300

    Pegando requisição e listando na home via card com FutureBuilder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pixlog_translater/services/translater_service.dart';
 import 'package:pixlog_translater/themes/app_colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,18 +11,49 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
+  TranslateService translateService = TranslateService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
         elevation: 0,
         title: const Text("Tradutor"),
         backgroundColor: AppColors.primary,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Tradutor'),
-      ),
+      body: FutureBuilder(
+        future: translateService.getResources(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.primary)),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data["resource"].length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                child: Card(
+                  child: Column(
+                    children: [
+                      Text(snapshot.data["resource"][index]["resource"]["created_at"]),
+                      Text(snapshot.data["resource"][index]["resource"]["updated_at"]),
+                      Text(snapshot.data["resource"][index]["resource"]["resource_id"]),
+                      Text(snapshot.data["resource"][index]["resource"]["module_id"]),
+                      Text(snapshot.data["resource"][index]["resource"]["value"]),
+                      Text(snapshot.data["resource"][index]["resource"]["language_id"]),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      )
     );
   }
 }
