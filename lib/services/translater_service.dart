d2353fa9f831563cabbee9f8e0f73e6cdf3a3380
Well Luiz <wellingtonluiiiz@gmail.com>
2022-05-18 16:06:47 -0300

    Primeira versão da função de pesquisa

--- lib/services/translater_service.dart
+++ lib/services/translater_service.dart
@@ -23,4 +23,14 @@ class TranslateService {
       "result": false,
     };
   }
+
+  searchResource(resources,searchName) {
+    var filteredResources = [];
+    for(int i=0;i<resources.length;i++){
+      if(resources[i]["resource"]["resource_id"].toLowerCase().contains(searchName.toLowerCase())) {
+        filteredResources.add(resources[i]);
+      }
+    }
+    return filteredResources;
+  }
 }
